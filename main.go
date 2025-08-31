7bef0a4ab08fb0171d5ab873329a9c071e8a2ba3
cth.release <cth.release@gmail.com>
2025-08-31 11:22:21 +0900

    [update] main.go

--- main.go
+++ main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 
-	"cth.release/web"
-	"github.com/gofiber/fiber/v2"
+	utils "cth.release/common/utils"
+	web "cth.release/web"
 )
 
 func main() {
-	app := fiber.New()
-	web.SetupRoutes(app)
+	port := flag.String("PORT", "8080", "PORT Integer Default: 8080")
+	kubeConfig := flag.String("PORT", "~/.kube/config", "kubernetes config path Default: ~/.kube/config")
 
-	err := app.Listen(":3000")
+	flag.Parse()
+
+	Port, err := strconv.Atoi(*port)
+
 	if err != nil {
-		log.Fatalf("Error Starting Server: %v", err)
+		fmt.Println("Error: --PORT is Not Integer")
+		flag.Usage()
+		os.Exit(1)
 	}
+
+	if Port > 65535 || Port < 1 {
+		fmt.Println("Error: --PORT is Invalid ex(Port > 65535 or Port < 1)")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	err = utils.SetConfig(&utils.Config{
+		Port:       Port,
+		KubeConfig: *kubeConfig,
+	})
+
+	if err != nil {
+		log.Fatalf("Error saving config : %s", err.Error())
+	}
+
+	config := utils.GetConfig()
+
+	app := web.InitServer(config)
+
+	if app == nil {
+		log.Fatalln("Init Server Error")
+		return
+	}
+
+	app.App.Listen(fmt.Sprintf(":%d", config.Port))
 }
