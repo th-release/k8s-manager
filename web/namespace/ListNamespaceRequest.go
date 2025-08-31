e1c33267b40af2fb300c7dd07f99d535459c71ff
cth.release <cth.release@gmail.com>
2025-08-31 15:33:06 +0900

    [update] bug fix

--- web/namespace/ListNamespaceRequest.go
+++ web/namespace/ListNamespaceRequest.go
@@ -1,30 +1,12 @@
 package namespace
 
 import (
-	"strings"
-
 	"cth.release/common/kubernetes"
 	"cth.release/common/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
 func ListNamespaceRequest(c *fiber.Ctx) error {
-	var dto ListNamespaceRequestDto
-	if err := c.QueryParser(&dto); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(utils.BasicResponse{
-			Success: false,
-			Message: err.Error(),
-			Data:    nil,
-		})
-	}
-
-	if len(strings.Trim(dto.Namespace, "")) <= 0 {
-		return c.Status(400).JSON(utils.BasicResponse{
-			Success: false,
-			Message: "Please enter a valid namespace value.",
-		})
-	}
-
 	client, err := kubernetes.NewK8sClient()
 
 	if err != nil {
